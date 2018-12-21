7c8fb67c8820f6e23147f8a5e5ea053b8305a2cf
user140592 <user140592@eu201.local>
2018-12-21 13:47:32 +0000

    Initial Commit

---
+++ Control/Control.h
@@ -0,0 +1,19 @@
+//
+//  Control.h
+//  Control
+//
+//  Created by user140592 on 12/21/18.
+//  Copyright © 2018 talspektor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Control.
+FOUNDATION_EXPORT double ControlVersionNumber;
+
+//! Project version string for Control.
+FOUNDATION_EXPORT const unsigned char ControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Control/PublicHeader.h>
+
+
